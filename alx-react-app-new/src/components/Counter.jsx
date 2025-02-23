0baa3e6d78e0ceee63ea3f144a974554bb1a97c3
laraphasha <rapelanglara23@gmail.com>
2025-02-23 16:39:00 +0200

    added counter

---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  let countPlus = count + 1;
+  let countMinus = count - 1;
+
+  return (
+    <counter style={{border: '1px solid gray', padding: '10px', margin: '10px'}}>
+      <div>
+        <button onClick={() => {setCount(countPlus)}}>
+          Count+
+        </button>
+        <button onClick={() => {setCount(countMinus)}}>
+          Count-
+        </button>
+        <button onClick={() => {setCount(0)}}>
+          Reset
+        </button>
+      </div>  
+      <p>{count}</p>
+    </counter>
+  );
+}
+
+export default Counter;
