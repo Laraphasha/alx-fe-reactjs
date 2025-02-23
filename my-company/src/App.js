ed94b932158f4cb35241d3fce7fcf1c74c8d93d6
laraphasha <rapelanglara23@gmail.com>
2025-02-24 00:00:35 +0200

    update

--- my-company/src/App.js
+++ my-company/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
 
 function App() {
   return (
