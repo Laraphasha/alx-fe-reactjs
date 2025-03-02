d1b933ee36f48bf41a42e91e612e6342019e2f28
laraphasha <rapelanglara23@gmail.com>
2025-03-02 13:24:53 +0200

    navbar

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
